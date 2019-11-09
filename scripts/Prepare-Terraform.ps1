657d16d3d7a8b4ae9310c92d6b2ae10ca0d4b631
Steve Baker <steve_m_baker@hotmail.com>
2019-11-09 15:11:50 +0000

    Added env vars for Terraform

--- scripts/Prepare-Terraform.ps1
+++ scripts/Prepare-Terraform.ps1
@@ -9,6 +9,8 @@ foreach ($envvar in (Get-ChildItem env:)) {
     $envVarHash.Add("$($tokenPrefix)$($envvar.Name)$($tokenSuffix)", $envvar.Value)
 }
 
+$envVarHash
+
 # Get files
 $targetFiles = (Get-ChildItem -Path $targetFilePattern)
 
@@ -18,3 +20,9 @@ foreach ($targetFile in $targetFiles) {
         ((Get-Content -Path $targetFile -Raw) -replace $item.key, $item.value) | Set-Content -Path $targetFile
     }
 }
+
+# Add env vars for Terraform
+$env:ARM_SUBSCRIPTION_ID = $env:AZURE_SUBSCRIPTION_ID
+$env:ARM_CLIENT_ID = $env:AZURE_CLIENT_ID
+$env:ARM_CLIENT_SECRET = $env:AZURE_CLIENT_SECRET
+$env:ARM_TENANT_ID = $env:AZURE_TENANT_ID
