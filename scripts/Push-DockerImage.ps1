40123e927bcd0d4a341729f3f171eec0abc37079
Steve Baker <steve_m_baker@hotmail.com>
2019-11-19 17:42:35 +0000

    Changed formatting

--- scripts/Push-DockerImage.ps1
+++ scripts/Push-DockerImage.ps1
@@ -1,10 +1,15 @@
 Push-Location .\app
+
 # Logging in to container registry
 az acr login --name $env:CONTAINER_REGISTRY_NAME
+
 # Getting the fqdn for the acr
 $acr_fqdn = (az acr list --query [0].loginServer -o tsv)
+
 # Tagging the app to the ACR
 docker tag steevaavoo/nodeapp $acr_fqdn/samples/nodeapp
+
 # Pushing the image to the ACR
 docker push $acr_fqdn/samples/nodeapp
+
 Pop-Location
