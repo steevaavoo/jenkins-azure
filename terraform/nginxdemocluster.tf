f1932ce99dc3e18bcaa9f0bbc40816ded59a5889
Steve Baker <steve_m_baker@hotmail.com>
2019-11-22 15:58:04 +0000

    Removed unnecessary string interpolation syntax

--- terraform/nginxdemocluster.tf
+++ terraform/nginxdemocluster.tf
@@ -9,33 +9,33 @@ terraform {
   }
 }
 resource "azurerm_resource_group" "stvrg" {
-  name     = "${var.azure_resourcegroup_name}"
-  location = "${var.location}"
+  name     = var.azure_resourcegroup_name
+  location = var.location
 }
 resource "azurerm_container_registry" "stvacr" {
-  name                = "${var.container_registry_name}"
-  resource_group_name = "${azurerm_resource_group.stvrg.name}"
-  location            = "${azurerm_resource_group.stvrg.location}"
-  admin_enabled       = "${var.acr_admin_enabled}"
-  sku                 = "${var.acr_sku}"
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.stvrg.name
+  location            = azurerm_resource_group.stvrg.location
+  admin_enabled       = var.acr_admin_enabled
+  sku                 = var.acr_sku
 }
 
 resource "azurerm_kubernetes_cluster" "stvaks" {
-  name                = "${var.azurerm_kubernetes_cluster_name}"
-  location            = "${azurerm_resource_group.stvrg.location}"
-  resource_group_name = "${azurerm_resource_group.stvrg.name}"
-  dns_prefix          = "${var.aks_dns_prefix}"
+  name                = var.azurerm_kubernetes_cluster_name
+  location            = azurerm_resource_group.stvrg.location
+  resource_group_name = azurerm_resource_group.stvrg.name
+  dns_prefix          = var.aks_dns_prefix
 
   agent_pool_profile {
-    name            = "${var.agent_pool_profile_name}"
-    count           = "${var.agent_pool_count}"
-    vm_size         = "${var.agent_pool_profile_vm_size}"
-    os_type         = "${var.agent_pool_profile_os_type}"
-    os_disk_size_gb = "${var.agent_pool_profile_disk_size_gb}"
+    name            = var.agent_pool_profile_name
+    count           = var.agent_pool_count
+    vm_size         = var.agent_pool_profile_vm_size
+    os_type         = var.agent_pool_profile_os_type
+    os_disk_size_gb = var.agent_pool_profile_disk_size_gb
   }
   service_principal {
-    client_id     = "${var.service_principal_client_id}"
-    client_secret = "${var.service_principal_client_secret}"
+    client_id     = var.service_principal_client_id
+    client_secret = var.service_principal_client_secret
   }
 
   tags = {
@@ -44,9 +44,9 @@ resource "azurerm_kubernetes_cluster" "stvaks" {
 }
 
 # output "client_certificate" {
-#   value = "${azurerm_kubernetes_cluster.stvaks.kube_config.0.client_certificate}"
+#   value = azurerm_kubernetes_cluster.stvaks.kube_config.0.client_certificate
 # }
 
 # output "kube_config" {
-#   value = "${azurerm_kubernetes_cluster.stvaks.kube_config_raw}"
+#   value = azurerm_kubernetes_cluster.stvaks.kube_config_raw
 # }
