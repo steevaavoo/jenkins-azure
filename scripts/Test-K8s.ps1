ee2406db28221ea21e6389cd74f3208c1bea0601
Steve Baker <steve_m_baker@hotmail.com>
2019-12-14 12:30:24 +0000

    Added kubectl context

--- scripts/Test-K8s.ps1
+++ scripts/Test-K8s.ps1
@@ -1,2 +1,5 @@
+# Setting k8s current context
+az aks get-credentials --resource-group $env:AKS_RG_NAME --name $env:AKS_CLUSTER_NAME --overwrite-existing
+
 # Testing kubectl
 kubectl version --short
