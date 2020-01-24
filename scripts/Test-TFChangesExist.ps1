d3ce8c7a06218bf11840ab95870702edd437e914
Adam Rush <adamrushuk@gmail.com>
2020-01-24 18:22:44 +0000

    Changed to use Verbose

--- scripts/Test-TFChangesExist.ps1
+++ scripts/Test-TFChangesExist.ps1
@@ -3,9 +3,9 @@ Push-Location -Path .\terraform
 
 # Check for planned changes in Terraform diff file
 $message = "Checking for Terraform planned changes"
-Write-Output "`nSTARTED: $message..."
+Write-Verbose "`nSTARTED: $message..."
 if (Get-Content diff.txt | Select-String "Plan: 0 to add, 0 to change, 0 to destroy") { $false } else { $true }
-Write-Output "FINISHED: $message."
+Write-Verbose "FINISHED: $message."
 
 # Revert to previous folder location
 Pop-Location
