d88ffbbe1432169a05548a731accd1edc10e4701
Adam Rush <adamrushuk@gmail.com>
2020-01-25 09:00:29 +0000

    Added preference vars to capture verbose stream

--- scripts/Test-TFChangesExist.ps1
+++ scripts/Test-TFChangesExist.ps1
@@ -2,6 +2,14 @@
 #
 # IMPORTANT: when using "returnStdout: true" in Jenkins, you MUST ensure ONLY your desired value is output to the
 # pipeline. DO NOT use Write-Output, Write-Host etc.
+#
+# If you are using the returnStdout option of the powershell Pipeline step then only stream 1 will be returned,
+# while streams 2-6 will be redirected to the console output if you enable stream pref to "Continue"
+# Ref: https://jenkins.io/blog/2017/07/26/powershell-pipeline/
+
+# Set prefs
+$VerbosePreference = "Continue"
+$ErrorActionPreference = "Stop"
 
 # Change into TF folder location
 Push-Location -Path .\terraform
