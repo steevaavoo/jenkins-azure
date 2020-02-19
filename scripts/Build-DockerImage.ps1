1d0bae05d5e12a903023d9633e4bf66ebbcc0e74
Adam Rush <rusha103@mod.gov.uk>
2020-02-19 21:31:49 +0000

    Changed logic order

--- scripts/Build-DockerImage.ps1
+++ scripts/Build-DockerImage.ps1
@@ -6,9 +6,7 @@ az acr login --name $env:ACR_NAME
 # Skip build if tag already exists in acr
 $acrRepoTags = az acr repository show-tags --name $env:ACR_NAME --repository $env:CONTAINER_IMAGE_NAME | ConvertFrom-Json
 
-if (($env:CONTAINER_IMAGE_TAG -in $acrRepoTags) -or ($env:FORCE_CONTAINER_BUILD -eq "true")) {
-    Write-Output "SKIPPING: Building docker image via ACR...[CONTAINER_IMAGE_TAG '$env:CONTAINER_IMAGE_TAG'] already exists."
-} else {
+if (($env:CONTAINER_IMAGE_TAG -notin $acrRepoTags) -or $env:FORCE_CONTAINER_BUILD) {
     # Local build
     # Format: repo/image:tag
     # docker build . -t adamrushuk/nodeapp:latest
@@ -18,6 +16,8 @@ if (($env:CONTAINER_IMAGE_TAG -in $acrRepoTags) -or ($env:FORCE_CONTAINER_BUILD 
     Write-Output "STARTED: $message..."
     az acr build -t $env:CONTAINER_IMAGE_TAG_FULL -r $env:ACR_NAME .
     Write-Output "FINISHED: $message."
+} else {
+    Write-Output "SKIPPING: Building docker image via ACR...[CONTAINER_IMAGE_TAG '$env:CONTAINER_IMAGE_TAG'] already exists."
 }
 
 Pop-Location
