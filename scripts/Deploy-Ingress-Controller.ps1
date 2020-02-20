9d02d7f054d5f263ce0e4888eaecaa33eb5321fb
Adam Rush <rusha103@mod.gov.uk>
2020-02-20 12:37:24 +0000

    Added debug logging to nginx controller

--- scripts/Deploy-Ingress-Controller.ps1
+++ scripts/Deploy-Ingress-Controller.ps1
@@ -20,9 +20,11 @@ helm repo update
 helm install nginx-ingress stable/nginx-ingress `
     --namespace ingress-basic `
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux `
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux `
+    --set controller.extraArgs.v=3
 
 # [OPTIONAL] args
-    # --set controller.replicaCount=2 `
+# --set controller.extraArgs.v=3 `
+# --set controller.replicaCount=2 `
 
 kubectl get service -l app=nginx-ingress --namespace ingress-basic
